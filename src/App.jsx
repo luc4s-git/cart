0a2b6d43309e15f78537c835cc474a58780a5bfb
luc4s-git <lucassantos4332@hotmail.com>
2024-01-10 23:04:33 -0300

    reducer correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-// components
+// Components
 import Navbar from './Navbar';
 import CartContainer from './CartContainer';
 import { useGlobalContext } from './context/context';
+// Components
 
+// Data
 import cartItems from './data';
-
-import { useReducer } from 'react';
-import reducer from './reducer/reducer';
-import { INCREASE_ITEM } from './reducer/action';
+// Data
 
 const defaultState = {
-  cartItems: cartItems,
+  cartItems: new Map(),
 };
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, defaultState);
-
-  const increaseItem = () => {
-    dispatch({ type: INCREASE_ITEM });
-  };
-
   return (
     <main>
       <Navbar />
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,13 +1,37 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useReducer, useState } from 'react';
+
+// reducer actions
+import {
+  INCREASE_ITEM,
+  DECREASE_ITEM,
+  REMOVE_ITEM,
+  CLEAR_CART,
+} from '../reducer/action';
+
+// reducer
+import reducer from '../reducer/reducer';
+
+import cartItems from '../data';
+
+const defaultState = {
+  cartItems: new Map(),
+};
 
 const GlobalContext = createContext();
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
 const AppContext = ({ children }) => {
-  const cu = null;
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  const increaseItem = () => {
+    dispatch({ type: INCREASE_ITEM });
+  };
+
   return (
-    <GlobalContext.Provider value={{ cu }}>{children}</GlobalContext.Provider>
+    <GlobalContext.Provider value={{ increaseItem }}>
+      {children}
+    </GlobalContext.Provider>
   );
 };
 
