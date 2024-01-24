2d8248c0e4e460a222d62f6a73fa0f89fb6cc259
luc4s-git <lucassantos4332@hotmail.com>
2024-01-24 17:52:34 -0300

    decrease funcionality correction

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -33,15 +33,16 @@ const reducer = (state, action) => {
     const newCart = new Map(state.cart);
     const itemId = action.payload.id;
     const item = newCart.get(itemId);
-    const newItem = {
-      ...item,
-      amount: item.amount === 0 ? 0 : item.amount - 1,
-    };
 
-    if (newItem.amount === 0) {
+    if (item.amount === 1) {
       newCart.delete(itemId);
       return { ...state, cart: newCart };
     }
+
+    const newItem = {
+      ...item,
+      amount: item.amount - 1,
+    };
 
     newCart.set(itemId, newItem);
     return { ...state, cart: newCart };
